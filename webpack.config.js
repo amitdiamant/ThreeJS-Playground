732d347869cc25a8e2dae6258341e312bb4fa895
Amit Diamant <amit.diamant@gmail.com>
2018-05-15 22:18:59 +0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlwebpackPlugin = require('html-webpack-plugin');
+const OpenBrowserPlugin = require('open-browser-webpack-plugin')
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        exclude: [/node_modules/],
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      inject: 'body',
+      template: './src/index.html'
+    }),
+    new OpenBrowserPlugin({
+      url: 'http://localhost:8080'
+    })
+  ],
+  devtool: 'cheap-module-source-map'
+};
